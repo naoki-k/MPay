73d81f97c9a668563cedafea2872adf517d8db56
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-08-14 10:42:01 +0900

    corporate informationのテストを作成

--- spec/models/corporate_informations_spec.rb
+++ spec/models/corporate_informations_spec.rb
@@ -2,9 +2,28 @@ require "rails_helper"
 
 RSpec.describe CorporateInformation, type: :model do
   describe "#valid?" do
-    let(:corporate_user) { build(:corporate_user) }
-    let(:corporate_information) { build(:corporate_information, corporate_user: corporate_user) }
+    context "when validation with correct params" do
+      let(:corporate_information) { build(:corporate_information) }
 
-    it { expect(corporate_information).to be_valid }
+      it { expect(corporate_information).to be_valid }
+    end
+
+    context "when adress is blank" do
+      let(:corporate_information) { build(:corporate_information, address: "") }
+
+      it { expect(corporate_information).not_to be_valid }
+    end
+
+    context "when no relation for corporate_user" do
+      let(:corporate_information) { build(:corporate_information, corporate_user: nil) }
+
+      it { expect(corporate_information).not_to be_valid }
+    end
+
+    context "when parameters length are wrong" do
+      let(:corporate_information) { build(:corporate_information, detail: "a" * 65536) }
+
+      it { expect(corporate_information).not_to be_valid }
+    end
   end
 end
