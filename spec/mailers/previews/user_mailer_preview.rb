342d8569ed4095a7497f36e5b732af83d1d48637
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-08-21 14:21:10 +0900

    mailerをプレビューできるように修正

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -2,10 +2,30 @@
 # mailers can preview in http://localhost:3000/rails/mailers
 class UserMailerPreview < ActionMailer::Preview
   def welcome_email
-    UserMailer.welcome_email(PersonalUser.first)
+    UserMailer.welcome_email(user_with_token)
   end
 
   def account_activation
-    UserMailer.account_activation(PersonalUser.first)
+    UserMailer.account_activation(user_with_token)
   end
+
+  private
+
+    def user_with_token
+      @user ||= User.find_by(email: "preview@example.com")
+      @user ||= PersonalUser.create({
+        name: "preview",
+        tel: "123-4567-8910",
+        email: "preview@example.com",
+        password: "password",
+        password_confirmation: "password",
+        activation_digest: BCrypt::Password.create(token)
+      })
+      @user.activation_token = token
+      @user
+    end
+
+    def token
+      "tekitouna-token-desu"
+    end
 end
