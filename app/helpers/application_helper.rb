7dad79a00cdf7ed5ec276380b65fbb6dccdca371
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-08-21 17:59:01 +0900

    サブタイトルとして新規登録を表示させるためのhelper作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def signup_title(path)
+    if [new_users_admin_path, new_users_personal_path, new_users_corporate_path].include?(path)
+      "新規登録"
+    end
+  end
 end
