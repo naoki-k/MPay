00679193f7854497511aa72e8c882e999907a2fb
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-08-22 13:59:21 +0900

    サブタイトル表示用のhelper削除

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-  def signup_title(path)
-    if [new_users_admin_path, new_users_personal_path, new_users_corporate_path].include?(path)
-      "新規登録"
-    end
-  end
 end
