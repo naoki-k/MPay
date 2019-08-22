9ecbcfc90140eaf2c3bb8018c30e6f0c3d810ec4
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-08-22 17:38:39 +0900

    バリデーションを同時にかけるように変更

--- app/controllers/users/corporates_controller.rb
+++ app/controllers/users/corporates_controller.rb
@@ -1,6 +1,7 @@
 class Users::CorporatesController < ApplicationController
   def new
     @user = CorporateUser.new
+    @information = CorporateInformation.new
   end
 
   def show
@@ -14,11 +15,12 @@ class Users::CorporatesController < ApplicationController
   def create
     @user = CorporateUser.new(user_params)
     @information = CorporateInformation.new(information_params)
-    if @user.save
+    @information&.corporate_user = @user
+    if ([@user, @information].map(&:valid?)).all?
+      @user.save
+      @information.save
       UserMailer.account_activate_request(@user).deliver_now
       flash[:success] = "アカウント作成の申請をしました。"
-      @information.corporate_user = @user
-      flash[:info] = "企業情報の登録に失敗しました。" unless @information.save
       redirect_to new_users_corporate_url
     else
       flash.now[:danger] = "アカウントの作成に失敗しました。"
