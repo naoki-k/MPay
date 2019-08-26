029331749752ec8e2f6ad9fa5cc916072ef6c403
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-08-26 12:18:33 +0900

    trade情報確認の分岐をコントローラに移行

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -13,15 +13,25 @@ class TradesController < ApplicationController
         flash[:success] = "取引が正常に行われました。"
         redirect_to new_trade_url
       else
-        flash[:error] = "取引に失敗しました。"
+        flash[:danger] = "取引に失敗しました。"
         redirect_to new_trade_url
       end
     end
   end
 
   def confirmation
-    @user = User.find_by(code: params[:target_user_code])
-    @amount = params[:amount]
-    render
+    if params[:confirmation][:target_user_code].blank? || params[:confirmation][:amount].blank?
+      flash.now[:danger] = "未入力項目があります"
+      render :new
+      return
+    end
+    @user = User.find_by(code: params[:confirmation][:target_user_code])
+    @amount = params[:confirmation][:amount]
+    if @user
+      render
+    else
+      flash.now[:danger] = "ユーザーが見つかりません。"
+      render :new
+    end
   end
 end
