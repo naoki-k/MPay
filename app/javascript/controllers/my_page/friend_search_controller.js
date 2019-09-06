aec598f507efeee97bf924109968b4df59c325ef
Naoki Kakuta <naoki.k.0820.i@gmail.com>
2019-09-06 18:27:03 +0900

    表示非表示を切り替える

--- app/javascript/controllers/my_page/friend_search_controller.js
+++ app/javascript/controllers/my_page/friend_search_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "keyword", "result" ]
+  static targets = [ "keyword", "result", "tab", "content" ]
 
   search() {
     const data = new FormData();
@@ -12,6 +12,10 @@ export default class extends Controller {
     })
 
     fetch(request).then((response) => {
+      this.resultTarget.setAttribute("aria-disabled", false)
+      this.contentTargets.forEach((element) => {
+        element.setAttribute('aria-disabled', true)
+      })
       return response.json()
     }).then((data) => {
       this.resultTarget.innerHTML = ""
@@ -20,6 +24,14 @@ export default class extends Controller {
       })
     }).catch(() => {
       this.resultTarget.innerHTML = "<div>not found</div>"
+    })
+  }
+
+  slide(event) {
+    this.resultTarget.setAttribute('aria-disabled', true)
+    const index = this.tabTargets.indexOf(event.currentTarget)
+    this.contentTargets.forEach((element, i) => {
+      element.setAttribute('aria-disabled', i !== index)
     })
   }
 }
